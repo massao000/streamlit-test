a4ea57ef4e73a31d9f55a9bb97a411461f2a6db6
massao000 <69783019+massao000@users.noreply.github.com>
2021-12-06 16:57:17 +0900

    Update folder.py

--- folder.py
+++ folder.py
@@ -20,5 +20,5 @@ if b2:
 st.write(glob.glob(f'{os.path.dirname(__file__)}'))
 st.write(glob.glob("scr/*"))
 st.write(glob.glob("test/*"))
-for i in glob.glob("scr/*.mp"):
+for i in glob.glob("scr/*.mp4"):
     st.audio(i)
