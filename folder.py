c607ff12aa06bc8756236605d960cc13d4aa5e43
massao000 <69783019+massao000@users.noreply.github.com>
2021-12-06 14:57:57 +0900

    Add files via upload

---
+++ folder.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import os
+import glob
+import uuid
+
+b1 = st.button("test")
+
+if b1:
+    x = str(uuid.uuid1())
+    os.mkdir(x)
+
+
+st.write(glob.glob(f'{os.path.dirname(__file__)}\*'))
