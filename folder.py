b10be06edccf519663f1772d7faecaafd75df069
massao000 <69783019+massao000@users.noreply.github.com>
2021-12-06 15:40:18 +0900

    Update folder.py

--- folder.py
+++ folder.py
@@ -13,7 +13,7 @@ if b1:
 if b2:
     y = glob.glob("scr/*")
     for i in y:
-        os.remove(y)
+        os.remove()
 
 st.write(glob.glob(f'{os.path.dirname(__file__)}'))
 st.write(glob.glob("scr/*"))
